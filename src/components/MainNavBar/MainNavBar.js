e59212c0f34c56fcc38daaac80b47f19fc0c10e2
KiwilycReal <kiwilyc9827@gmail.com>
2021-09-26 08:24:32 +1000

    Basic drawer logic completed

--- src/components/MainNavBar/MainNavBar.js
+++ src/components/MainNavBar/MainNavBar.js
@@ -1,61 +1,112 @@
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import AdbIcon from '@mui/icons-material/Adb';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
-import { useState } from 'react';
-import { AppBar, Button, IconButton, Toolbar, Typography, Box, Drawer,  List, ListItemIcon, Divider, ListItemText, CssBaseline } from '@material-ui/core';
-import { AccountCircle, ChevronLeft } from '@material-ui/icons';
-import MenuIcon from '@material-ui/icons/Menu';
 import styles from './MainNavBar.module.css';
 
-const MainNavBar = () => {
+const MainNavBar = (props) => {
     
-    const [isDrawerOpened, setIsDrawerOpened] = useState(false);
+    const menuItemObj = {
+        'Gallery': <PhotoLibraryIcon />,
+        'Account Books': <LocalAtmIcon />,
+        'Dummy1': <AdbIcon />,
+        'Dummy2': <AdbIcon />,
+        'Dummy3': <AdbIcon />,
+        'Dummy4': <AdbIcon />
+    }
 
     return <>
-        <AppBar position='fixed'
+        <AppBar component='div' position='fixed'
             sx={{ 
-                zIndex: 1300
+                zIndex: theme => theme.zIndex.drawer + 1,
+                transition: 'width 1s',
+                transitionTimingFunction: 'ease',
+                ...(props.isDrawerOpened && {
+                    marginLeft: props.openedDrawerWidth,
+                    width: `calc(100% - ${props.openedDrawerWidth})`
+                }),
             }}
         >
-            <Toolbar style={{
-                'justify-content': 'space-between'
+            <Toolbar sx={{
+                justifyContent: 'space-between'
             }}>
                 <Box sx={{
                     display: 'flex',
                     'align-items': 'center',
                 }}>
-                    <IconButton edge="start">
-                        <MenuIcon/>
+                    <IconButton edge="start"
+                        onClick={props.onDrawerChangeHandler}
+                    >
+                        <MenuIcon />
                     </IconButton>
                     <Typography
                         variant="h5"
-                        style={{margin: '0 0 0 1rem'}}
+                        sx={{margin: '0 0 0 1rem'}}
                     >
                         HOMEPORTAL ｜ 家庭中枢
                     </Typography>
                 </Box>
                 <Box sx={{
                     display: 'flex',
-                    'justify-content': 'flex-end'
+                    justifyContent: 'flex-end'
                 }}>
                     <IconButton
                         edge="end"
                         color="inherit"
                     >
-                        <AccountCircle/>
+                        <AccountCircleIcon />
                         Login
                     </IconButton>
                 </Box>
             </Toolbar>
         </AppBar>
-        <Drawer variant="permanent" open={isDrawerOpened} anchor="left">
-            {/* <IconButton onClick={()=>setIsDrawerOpened(false)}>
-                <ChevronLeft />
-            </IconButton> */}
-            <Toolbar />
-            <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                
-                <p>text</p>
-            ))}
+        <Drawer
+            variant="permanent"
+            anchor="left"
+            sx={{
+                '& .MuiDrawer-paper': {
+                    width: props.isDrawerOpened ? props.openedDrawerWidth : props.closedDrawerWidth,
+                    transition: 'width 1s',
+                    transitionTimingFunction: 'ease'
+                },
+            }}
+        >
+            <Toolbar>
+                <IconButton edge="end">
+                    <ChevronLeftIcon />
+                </IconButton>
+            </Toolbar>
+            <List sx={{
+                overflowX: 'hidden',
+                // marginTop: theme => theme.mixins.toolbar["@media (min-width:600px)"].minHeight + 'px'
+            }}>
+                {Object.keys(menuItemObj).map((label, index) => 
+                    <ListItem key={label}
+                        sx={{padding: '8px 4px'}}
+                    >
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {menuItemObj[label]}
+                            </ListItemIcon>
+                            <ListItemText primary={label} />
+                        </ListItemButton>
+                    </ListItem>
+                )}
             </List>
         </Drawer>
     </>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,37 @@
 
 import styles from './HomePage.module.css';
-import { Button } from '@material-ui/core';
+import { useState } from 'react';
+import Grid from '@mui/material/Grid'
 import MainNavBar from '../../components/MainNavBar/MainNavBar';
 import BottomBanner from '../../components/MainNavBar/BottomBanner/BottomBanner';
 
 const HomePage = () => {
+
+    const openedDrawerWidth = '239px';
+    const closedDrawerWidth = '62px';
+    const [isDrawerOpened, setIsDrawerOpened] = useState(false);
+
+    const onDrawerChange = () => {
+        setIsDrawerOpened(!isDrawerOpened)
+    }
+
     return <>
-        <MainNavBar/>
-        <div style={{height: '3000px'}}/>
+        <MainNavBar isDrawerOpened={isDrawerOpened}
+            onDrawerChangeHandler={onDrawerChange}
+            openedDrawerWidth={openedDrawerWidth}
+            closedDrawerWidth={closedDrawerWidth}
+        />
+        <Grid
+            sx={{
+            height: '3000px',
+            zIndex: theme => theme.zIndex.drawer + 1,
+            marginTop: theme => theme.mixins.toolbar["@media (min-width:600px)"].minHeight + 'px',
+            transition: 'margin 1s',
+            transitionTimingFunction: 'ease',
+            marginLeft: isDrawerOpened ? openedDrawerWidth : closedDrawerWidth
+        }}>
+            <h1>teste</h1>
+        </Grid>
         <BottomBanner/>
     </>
 };
